db7d7beeaa1b9f1fbd83a8cbf9685369d39d0733
LEETAEHO <tae1560@gmail.com>
2013-03-03 14:58:40 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnalystRank::Application.config.secret_token = '5785a9564a3a63629e90d8469b95866c6bc4adff998df27f151c3805b1902e546f2147a39886a2eafd8233b33c67bbc55dde94352e008d8bb735febccab30884'
