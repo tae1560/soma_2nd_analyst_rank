75f1da41f8eb88587a39e34cd2a3f92bb143c25a
LEETAEHO <tae1560@gmail.com>
2013-05-16 20:51:16 +0900

    Added push_messages_on_devices controller

--- app/controllers/push_messages_on_devices_controller.rb
+++ app/controllers/push_messages_on_devices_controller.rb
@@ -1,5 +1,5 @@
 class PushMessagesOnDevicesController < ApplicationController
   def index
-    @push_messages_on_devices = PushMessagesOnDevices.all
+    @push_messages_on_devices = PushMessagesOnDevice.order("id DESC").all
   end
 end
