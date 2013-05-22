720048c77fd87f1ac2fc64e869aa4ea0cc429b3f
tprpc <tprpc@tprpc.com>
2013-05-22 11:58:49 +0900

    홈 컨트롤러 뷰 추가/변경, 라우트 수정, 디자인 수정

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ AnalystRank::Application.routes.draw do
   #authenticated :user do
   #  root :to => 'home#index'
   #end
-  #root :to => "home#index"
-  root :to => "stock_firms#index"
+  root :to => "home#index"
+  #root :to => "stock_firms#index"
   devise_for :users, :controllers => {
       :omniauth_callbacks => "users/omniauth_callbacks"
   }
@@ -14,6 +14,7 @@ AnalystRank::Application.routes.draw do
   resources :recommendations
   resources :stock_firms
   resources :gcm_devices
+  resources :home
   resources :app_info
   resources :push_messages_on_devices
   resources :user_subscribe_stock_firms
