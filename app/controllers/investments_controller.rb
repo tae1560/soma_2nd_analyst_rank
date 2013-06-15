4c4242940bec53b52164b48692d232e26215a4ac
LEETAEHO <tae1560@gmail.com>
2013-06-15 20:23:45 +0900

    Calculate temp datas

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,3 +1,4 @@
+# coding: UTF-8
 class InvestmentsController < ApplicationController
   before_filter :authenticate_user!
 
@@ -8,6 +9,47 @@ class InvestmentsController < ApplicationController
 
   def show
     @investment = current_user.investments.find_by_id(params[:id])
+    @recommendations = @investment.stock_firm.recommendations.where("in_date > '#{@investment.start_date - 50.days}'").order("in_date DESC")
+
+    @keep_period = @investment.keep_period
+    @recent_period = @investment.recent_period
+    @loss_cut = @investment.loss_cut
+
+    @rest_asset = @investment.total_asset
+
+    @profit_asset = 0
+
+    @recommendation_prints = []
+    @recommendations.each do |recommendation|
+      recommendation_print = {}
+      in_day_candle = recommendation.get_in_day_candle
+      out_day_candle = recommendation.get_out_day_candle @keep_period.days.days
+      #profit = recommendation.get_profit @keep_period.days.days
+
+
+      recommendation_print[:stock_code_name] = recommendation.stock_code.name.gsub("보통주","")
+      recommendation_print[:state] = if out_day_candle then "매도완료" else if in_day_candle then "보유중" else "구매대기" end end
+
+      real_invest_asset = [@investment.invest_asset, @rest_asset].min
+      recommendation_print[:volumn] = if in_day_candle then real_invest_asset / in_day_candle.open else 0 end
+
+      @rest_asset -= in_day_candle.open * recommendation_print[:volumn]
+
+      if out_day_candle then @rest_asset += out_day_candle.open * recommendation_print[:volumn] end
+
+      recommendation_print[:symbol] = recommendation.symbol
+      recommendation_print[:stock_firm_name] = recommendation.stock_firm.name
+      recommendation_print[:in_date] = Utility.utc_datetime_to_kor_str recommendation.in_date
+      recommendation_print[:in_day_candle_date] = in_day_candle && Utility.utc_datetime_to_kor_str(in_day_candle.trading_date) || "-"
+      recommendation_print[:in_day_candle_open] = in_day_candle && in_day_candle.open || "-"
+      recommendation_print[:out_day_candle_date] = out_day_candle && Utility.utc_datetime_to_kor_str(out_day_candle.trading_date) || "-"
+      recommendation_print[:out_day_candle_close] = out_day_candle && out_day_candle.close || "-"
+      recommendation_print[:profit] = if in_day_candle and out_day_candle then (out_day_candle.close - in_day_candle.open) * recommendation_print[:volumn] else "-" end
+      unless recommendation_print[:profit] == "-" then @profit_asset += recommendation_print[:profit] end
+      @recommendation_prints.push recommendation_print
+    end
+
+
   end
 
   def new
